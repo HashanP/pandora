5905198da184fce506d77c80e7cdaa41882662f1
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-11-19 18:49:22 +0000

    Updated analytics

--- scripts/courses.js
+++ scripts/courses.js
@@ -937,8 +937,7 @@ page(function(e,next) {
 				window.XMLHttpRequest.prototype.open = proxied;
 			} else {
 				process.env.USER = achilles.User.parse(body);
-				ga('create', 'UA-56908379-1', {userId:process.env.USER._id});
-				ga('send', 'pageview');
+				ga("set", "&uid", process.env.USER._id); // Set the user ID using signed-in user_id.
 				document.body.classList.add("loggedIn");
 				document.body.innerHTML = m({user:process.env.USER, admin: process.env.USER.roles.indexOf("admin") !== -1});
 				document.querySelector(".dropdown-toggle").addEventListener("click", function(e) {
