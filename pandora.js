97b388120ddd0605b33f5b121df3efdf32e49c35
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-12-28 15:23:29 +0000

    Initial commit

---
+++ pandora.js
@@ -0,0 +1,94 @@
+Courses = new Mongo.Collection("courses");
+
+var Schemas = {};
+
+Schemas.Course = new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 10
+  },
+  icon: {
+    type: String,
+    label: "Icon",
+    allowedValues:["French", "Latin", "Computing", "Art", "English", "Mathematics"]
+  }
+});
+
+Courses.attachSchema(Schemas.Course);
+
+if (Meteor.isClient) {
+  Template.login.events({
+    "submit .login-form": function(e) {
+      var username = e.target.username.value;
+      var password = e.target.password.value;
+
+      Meteor.loginWithPassword(username, password, function(err){
+        if(err) {
+          Session.set("err", "Your username or password is incorrect.");
+        } else {
+          Session.set("err", "");
+        }
+      });
+
+      return false;
+    }
+  });
+
+  Template.subjects.helpers({
+    "courses": function() {
+      return Courses.find({}, {sort:{icon:1}});
+    }
+  });
+
+  Template.navbar.helpers({
+    "username":function() {
+      return Meteor.user().emails[0].address.split("@")[0];
+    },
+    "admin": function() {
+      return Meteor.user().roles.indexOf("admin") !== "admin";
+    }
+  });
+
+  Router.onBeforeAction(function() {
+    if (! Meteor.userId()) {
+      this.render('login');
+    } else {
+      if(this.route._path.slice(0, 6) !== "/admin") {
+        this.layout("main");
+      }
+      this.next();
+    }
+  });
+
+  Router.route('/', function() {
+    this.render('subjects');
+  });
+
+  Router.route('/courses/:id/blog', function() {
+    console.log(this.params.id);
+    this.render("blog_list", {data: function() {
+      return Courses.findOne(this.params.id);
+    }});
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  Accounts.config({restrictCreationByEmailDomain:'whsb.essex.sch.uk'});
+}
+
+
+this.AdminConfig = {
+  name:"Pandora",
+  adminEmails:["10punchihewah@whsb.essex.sch.uk"],
+  collections: {
+    Users: {
+    },
+    Courses: {
+    }
+  }
+};
