99846140495da14158428e4e250898fd75635319
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-12-29 13:57:58 +0000

    Updated styles && Added create post form

--- pandora.js
+++ pandora.js
@@ -1,17 +1,48 @@
 Courses = new Mongo.Collection("courses");
 
+UI.registerHelper('eq', function(v1, v2, options) {
+  if(v1 == v2){
+    return true
+  } else {
+    return false
+  }
+});
+
+UI.registerHelper("i", function(obj) {
+  if(!obj) return null;
+  obj.forEach(function(item, i) {
+    item.index = i;
+  });
+  return obj;
+});
+
 var Schemas = {};
 
+Schemas.Post = new SimpleSchema({
+  "title": {
+    type: String
+  },
+  "content": {
+    type: String
+  },
+  "type": {
+    type: String
+  }
+});
+
 Schemas.Course = new SimpleSchema({
-  title: {
+  "title": {
     type: String,
     label: "Title",
     max: 10
   },
-  icon: {
+  "icon": {
     type: String,
     label: "Icon",
     allowedValues:["French", "Latin", "Computing", "Art", "English", "Mathematics"]
+  },
+  "posts": {
+    type: [Schemas.Post]
   }
 });
 
@@ -43,34 +74,83 @@ if (Meteor.isClient) {
 
   Template.navbar.helpers({
     "username":function() {
-      console.log(Meteor.user());
-      return Meteor.user().emails[0].address.split("@")[0];
+      if(Meteor.user()) {
+        return Meteor.user().emails[0].address.split("@")[0];
+      }
     },
     "admin": function() {
-      return Meteor.user().roles.indexOf("admin") !== -1;
+      if(Meteor.user()) {
+        return Meteor.user().roles.indexOf("admin") !== -1;
+      }
     }
   });
 
+  Template.insertPost.helpers({
+    "schema": function() {
+      return Schemas.Post;
+    },
+    "type": function() {
+      return Session.get("type");
+    }
+  });
+
+  Template.insertPost.events({
+    "change #options": function(e) {
+      Session.set("type", e.target.value);
+    },
+    "submit form": function(e) {
+      console.log(this._id);
+      Courses.update(this._id, {
+          $push:
+            {
+              posts: {
+                title: e.target.title.value,
+                type: e.target.type.value,
+                content: (e.target.type.value === "rich" ? $("#editor").val() : e.target.content.value)
+              }
+            }
+          }
+        );
+      return false;
+    }
+  });
+
+  Template.insertPost.rendered = function() {
+    $("#editor").wysihtml5();
+  }
+
   Router.onBeforeAction(function() {
-    if (! Meteor.userId()) {
+    if (!Meteor.userId()) {
       this.render('login');
     } else {
-      if(this.route._path.slice(0, 6) !== "/admin") {
-        this.layout("main");
-      }
+
       this.next();
     }
   });
 
-  Router.route('/', function() {
-    this.render('subjects');
-  });
+  Router.onBeforeAction(function() {
+    this.layout("course", {
+      data: function() {
+        return Courses.findOne(this.params.id);
+      }
+    });
+    this.next();
+  }, {except:"subjects"});
+
+  Router.route('/', {name: "subjects"});
 
   Router.route('/courses/:id/blog', function() {
-    console.log(this.params.id);
-    this.render("blog_list", {data: function() {
-      return Courses.findOne(this.params.id);
-    }});
+    this.render("blog_list", {
+      data: function() {
+        return Courses.findOne(this.params.id);
+      }
+    });
+  });
+
+  Router.route('/courses/:id/blog/new', function() {
+    console.log("hero");
+    Session.set("type", "rich");
+    this.render("insertPost", {data: Courses.findOne(this.params.id)});
   });
 
   Router.route("/logout", function() {
